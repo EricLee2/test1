3f2e081b38341af246a97ee9b3b235387bebe007
EricLee2 <psyche22@gmail.com>
2019-01-02 11:34:20 +0900

    pylinttest 2

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 """This script prompts a user to enter a message to encode or decode
    using a classic Caeser shift substitution (3 letter shift)"""
-x = 1
-if x == 1:
+x_value = 1
+if x_value == 1:
     # indented four spaces
     print "x is 1."
